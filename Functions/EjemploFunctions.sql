2d64e93a4d7b39561b98741f96ab21ff218cf3ac
marcoslafoz <marcos.lafoz96@gmail.com>
2023-03-23 13:25:48 +0100

    23/03/2023 - 13:25

--- Functions/EjemploFunctions.sql
+++ Functions/EjemploFunctions.sql
@@ -6,16 +6,18 @@ DROP FUNCTION IF EXISTS contar_productos$$
 CREATE FUNCTION contar_productos(gama VARCHAR(50))
 RETURNS INT UNSIGNED
 BEGIN
+
 -- Paso 1. Declaramos una variable local
 DECLARE total INT UNSIGNED;
 -- Paso 2. Contamos los productos
 SET total = (
-SELECT COUNT(*)
-FROM producto
-WHERE producto.gama = gama);
+    SELECT COUNT(*)
+    FROM producto
+    WHERE producto.gama = gama
+);
 -- Paso 3. Devolvemos el resultado
 RETURN total;
+
 END
 
-$$
-DELIMITER ; 
+$$ DELIMITER ; 
